4136da4606b5c4e6632fd6ba535c990bcfdaa868
Jaroslav <music.jp.production@gmail.com>
2026-02-10 16:33:33 +0200

    Fixed-up Walnut to work with updated imgui

--- Walnut/Platform/GUI/Walnut/ApplicationGUI.cpp
+++ Walnut/Platform/GUI/Walnut/ApplicationGUI.cpp
@@ -264,7 +264,7 @@ static void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface
 
 	// Create SwapChain, RenderPass, Framebuffer, etc.
 	IM_ASSERT(g_MinImageCount >= 2);
-	ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, wd, g_QueueFamily, g_Allocator, width, height, g_MinImageCount);
+	ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, wd, g_QueueFamily, g_Allocator, width, height, g_MinImageCount, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
 }
 
 static void CleanupVulkan()
@@ -557,13 +557,14 @@ namespace Walnut {
 		init_info.Queue = g_Queue;
 		init_info.PipelineCache = g_PipelineCache;
 		init_info.DescriptorPool = g_DescriptorPool;
-		init_info.Subpass = 0;
+		init_info.PipelineInfoMain.Subpass = 0;
 		init_info.MinImageCount = g_MinImageCount;
 		init_info.ImageCount = wd->ImageCount;
-		init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+		init_info.PipelineInfoMain.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
 		init_info.Allocator = g_Allocator;
 		init_info.CheckVkResultFn = check_vk_result;
-		ImGui_ImplVulkan_Init(&init_info, wd->RenderPass);
+		init_info.PipelineInfoMain.RenderPass = wd->RenderPass;
+		ImGui_ImplVulkan_Init(&init_info);
 
 		// Load default font
 		ImFontConfig fontConfig;
@@ -588,8 +589,6 @@ namespace Walnut {
 			err = vkBeginCommandBuffer(command_buffer, &begin_info);
 			check_vk_result(err);
 
-			ImGui_ImplVulkan_CreateFontsTexture(command_buffer);
-
 			VkSubmitInfo end_info = {};
 			end_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
 			end_info.commandBufferCount = 1;
@@ -601,7 +600,6 @@ namespace Walnut {
 
 			err = vkDeviceWaitIdle(g_Device);
 			check_vk_result(err);
-			ImGui_ImplVulkan_DestroyFontUploadObjects();
 		}
 
 		// Load images
@@ -736,7 +734,7 @@ namespace Walnut {
 		{
 			ImGui::SuspendLayout();
 			{
-				ImGui::SetItemAllowOverlap();
+				ImGui::SetNextItemAllowOverlap();
 				const float logoHorizontalOffset = 16.0f * 2.0f + 48.0f + windowPadding.x;
 				ImGui::SetCursorPos(ImVec2(logoHorizontalOffset, 6.0f + titlebarVerticalOffset));
 				UI_DrawMenubar();
@@ -896,7 +894,7 @@ namespace Walnut {
 				if (width > 0 && height > 0)
 				{
 					ImGui_ImplVulkan_SetMinImageCount(g_MinImageCount);
-					ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, &g_MainWindowData, g_QueueFamily, g_Allocator, width, height, g_MinImageCount);
+					ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, &g_MainWindowData, g_QueueFamily, g_Allocator, width, height, g_MinImageCount, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
 					g_MainWindowData.FrameIndex = 0;
 
 					// Clear allocated command buffers from here since entire pool is destroyed
