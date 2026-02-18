ebe6c6c820371a23c58052226edf6ec2aee12290
Jaroslav <music.jp.production@gmail.com>
2026-02-18 13:39:28 +0200

    Fixed up the log still using old Hazel filename

--- Walnut/Source/Walnut/Core/Log.cpp
+++ Walnut/Source/Walnut/Core/Log.cpp
@@ -19,9 +19,9 @@ namespace Walnut {
 		if (!std::filesystem::exists(logsDirectory))
 			std::filesystem::create_directories(logsDirectory);
 
-		std::vector<spdlog::sink_ptr> hazelSinks =
+		std::vector<spdlog::sink_ptr> walnutSinks =
 		{
-			std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/HAZEL.log", true),
+			std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/WALNUT.log", true),
 #if WL_HAS_CONSOLE
 			std::make_shared<spdlog::sinks::stdout_color_sink_mt>()
 #endif
@@ -35,15 +35,15 @@ namespace Walnut {
 #endif
 		};
 
-		hazelSinks[0]->set_pattern("[%T] [%l] %n: %v");
+		walnutSinks[0]->set_pattern("[%T] [%l] %n: %v");
 		appSinks[0]->set_pattern("[%T] [%l] %n: %v");
 
 #if WL_HAS_CONSOLE
-		hazelSinks[1]->set_pattern("%^[%T] %n: %v%$");
+		walnutSinks[1]->set_pattern("%^[%T] %n: %v%$");
 		appSinks[1]->set_pattern("%^[%T] %n: %v%$");
 #endif
 
-		s_CoreLogger = std::make_shared<spdlog::logger>("WALNUT", hazelSinks.begin(), hazelSinks.end());
+		s_CoreLogger = std::make_shared<spdlog::logger>("WALNUT", walnutSinks.begin(), walnutSinks.end());
 		s_CoreLogger->set_level(spdlog::level::trace);
 
 		s_ClientLogger = std::make_shared<spdlog::logger>("APP", appSinks.begin(), appSinks.end());
